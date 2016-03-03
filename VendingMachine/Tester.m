d34b9afdb6e0c5bf81f1a4e19396bed85ad711d8
Travis <travis@travis.com>
2016-03-03 13:43:32 -0500

    Added more test cases for vending machine.

--- VendingMachine/Tester.m
+++ VendingMachine/Tester.m
@@ -20,18 +20,59 @@
     VendingMachine *vendingMachine = [[VendingMachine alloc] init];
     if (![vendingMachine coinInsertedAndAccepted:NICKEL])
         NSLog(@"Vending machine does not accept nickels");
+    if (vendingMachine.change != 0.05)
+        NSLog(@"Nickel did not add $0.05 to change");
+    if (vendingMachine.nickels != 1)
+        NSLog(@"Nickel did not stay in the machine");
+    if (vendingMachine.nickelsInInventory != 1)
+        NSLog(@"Nickel did not store in inventory");
+    
+    vendingMachine = nil;
+    vendingMachine = [[VendingMachine alloc] init];
     
     if (![vendingMachine coinInsertedAndAccepted:DIME])
         NSLog(@"Vending machine does not accept dimes");
+    if (vendingMachine.change != 0.10)
+        NSLog(@"Dime did not add $0.10 to change");
+    if (vendingMachine.dimes != 1)
+        NSLog(@"Dime did not stay in the machine");
+    if (vendingMachine.dimesInInventory != 1)
+        NSLog(@"Dime did not store in inventory");
+    
+    vendingMachine = nil;
+    vendingMachine = [[VendingMachine alloc] init];
     
     if (![vendingMachine coinInsertedAndAccepted:QUARTER])
         NSLog(@"Vending machine does not accept quarters");
+    if (vendingMachine.change != 0.25)
+        NSLog(@"Quarter did not add $0.25 to change");
+    if (vendingMachine.quarters != 1)
+        NSLog(@"Quarter did not stay in the machine");
+    if (vendingMachine.quartersInInventory != 1)
+        NSLog(@"Quarters did not store in inventory");
+    
+    vendingMachine = nil;
+    vendingMachine = [[VendingMachine alloc] init];
     
     if ([vendingMachine coinInsertedAndAccepted:OTHER_COIN])
         NSLog(@"Vending machine accepts other coins");
+    [self verifyTheVendingMachineIgnoredInvalidCoins:vendingMachine];
+    
+    vendingMachine = nil;
+    vendingMachine = [[VendingMachine alloc] init];
     
     if ([vendingMachine coinInsertedAndAccepted:453])
         NSLog(@"Vending machine accepts invalid data");
+    [self verifyTheVendingMachineIgnoredInvalidCoins:vendingMachine];
+}
+
+- (void)verifyTheVendingMachineIgnoredInvalidCoins:(VendingMachine *)vendingMachine
+{
+    if (vendingMachine.change > 0)
+        NSLog(@"Invalid coin has been added to vending machine change amount.");
+    
+    if (vendingMachine.quarters > 0 || vendingMachine.quartersInInventory > 0 || vendingMachine.dimes > 0 || vendingMachine.dimesInInventory > 0 || vendingMachine.nickelsInInventory > 0 || vendingMachine.nickels > 0)
+        NSLog(@"Invalid coin accepted in the machine");
 }
 
 
